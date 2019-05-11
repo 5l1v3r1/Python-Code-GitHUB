268c7a707836849f84567c2ec9b4208209ab86a9
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-12 00:22:59 +0200

    no images saved

--- sdr/waterfall_screen.py
+++ sdr/waterfall_screen.py
@@ -1,14 +1,10 @@
 from matplotlib import mlab as mlab
-
 from rtlsdr import RtlSdr
-
 import numpy as np
-
 from PIL import Image
-
 import time
-
 import pygame
+
 DISPLAY_WIDTH = 1280
 DISPLAY_HEIGHT = 720
 
@@ -24,13 +20,27 @@ image = []
 
 
 def get_data():
-    # samples = sdr.read_samples(256*1024)
     samples = sdr.read_samples(16*1024)
-    # use matplotlib to estimate and plot the PSD
-    power, psd_freq = mlab.psd(samples, NFFT=1024, Fs=sdr.sample_rate /
-                               1e6)
-    psd_freq = psd_freq + sdr.center_freq/1e6
-    image.append(power)
+    power, _ = mlab.psd(samples, NFFT=1024, Fs=sdr.sample_rate /
+                        1e6)
+
+    max_pow = 0
+    min_pow = 10
+
+    # search whole data set for maximum and minimum value
+    for dat in power:
+        if dat > max_pow:
+            max_pow = dat
+        elif dat < min_pow:
+            min_pow = dat
+
+    # update image data
+    imagelist = []
+    for dat in power:
+        imagelist.append(mymap(dat, min_pow, max_pow, 0, 255))
+    image.append(np.array(imagelist, np.ubyte))
+    print(image)
+    print(len(image))
     print("LIVE")
 
 
@@ -56,6 +66,8 @@ while not game_quit:
         if event.type == pygame.QUIT:
             game_quit = True
 
+    pass
+
     pygame.display.update()
     clock.tick(60)
 
@@ -64,34 +76,10 @@ pygame.quit()
 try:
     get_data()
 
-    max_pow = 0
-    min_pow = 10
-    largearray = np.zeros((1024), np.ubyte)[np.newaxis]
-    largearray = np.transpose(largearray)
-
-    # search whole data set for maximum and minimum value
-    for arr in image:
-        for dat in arr:
-            if dat > max_pow:
-                max_pow = dat
-            elif dat < min_pow:
-                min_pow = dat
-
-    # create image data
-    imagelist = []
-    for arr in image:
-        thislist = []
-        for dat in arr:
-            # map all values between max and min, as bytes
-            thislist.append(mymap(dat, min_pow, max_pow, 0, 255))
-        imagelist.append(thislist[round(len(
-            thislist)/2)-round(len(thislist)/8): round(len(thislist)/2)+round(len(thislist)/8)])
-    largearray = np.array(imagelist, np.ubyte)
-
-    im = Image.fromarray(largearray, mode='L')
-    t = time.time()
-    im.save(f"sdr/images/waterfall{t}.jpg")
-    im.save(f"sdr/images/waterfall{t}.bmp")
+    #im = Image.fromarray(largearray, mode='L')
+    #t = time.time()
+    # im.save(f"sdr/images/waterfall{t}.jpg")
+    # im.save(f"sdr/images/waterfall{t}.bmp")
 
 
 except KeyboardInterrupt:
