440bbbe5b6caaf29a102f4206c18f90454e65a73
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-12 20:56:30 +0200

    not working tone generator

--- keller_dsp/m_tone_generator.py
+++ keller_dsp/m_tone_generator.py
@@ -2,6 +2,8 @@
 With this, everyone can create a tone.
 """
 
+import math
+
 
 class ToneGenerator:
     """
@@ -15,7 +17,16 @@ class ToneGenerator:
         fs - sampling frequency
         f - frequency
         """
-        pass
+        self.__fs = fs
+        self.__f = f
+        self.__phasak = 0
+        self.__maxint = 4294967295
+        self.__hz = self.__maxint / self.__fs
 
     def output(self):
-        pass
+        """
+        Takes one step and outputs new value.
+        """
+        self.__phasak = self.__phasak + self.__f * self.__hz
+        self.__phasak = self.__phasak % self.__maxint
+        return math.cos(2*math.pi*self.__phasak/self.__maxint)*100
