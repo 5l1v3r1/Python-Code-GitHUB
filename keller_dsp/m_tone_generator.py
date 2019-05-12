050b19d643f6cec28be56032bc7e5bd82b2bef9e
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-12 20:24:54 +0200

    will generate tones

---
+++ keller_dsp/m_tone_generator.py
@@ -0,0 +1,16 @@
+class ToneGenerator:
+    """
+    With this, everyone can create a tone.
+    """
+
+    def __init__(self, fs, f):
+        """
+        Tone generator.
+        Parameters:
+        fs - sampling frequency
+        f - frequency
+        """
+        pass
+
+    def output(self):
+        pass
