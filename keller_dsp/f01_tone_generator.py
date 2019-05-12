305121c3a51246705d56f1a764e2aefa069b8adf
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-12 21:20:07 +0200

    still doesn't work properly

--- keller_dsp/f01_tone_generator.py
+++ keller_dsp/f01_tone_generator.py
@@ -9,32 +9,20 @@ import numpy as np
 import pyaudio
 import m_tone_generator
 
-
-def callback(in_data, frame_count, time_info, status):
-    """
-    Callback function.
-    """
-    data_out = []
-
-    for _ in range(frame_count):
-        data_out.append(TG.output())
-
-    return (np.array(data_out), pyaudio.paContinue)
+from matplotlib import pyplot as plt
 
 
 # initialize pyaudio
 CHUNK_SIZE = 2048  # samples per iteration
 CHANNELS = 1       # 1 channel = mono | 2 channels = stereo
-RATE = 44100       # sampling frequency in Hz
+RATE = 192000       # sampling frequency in Hz
 P_AUDIO = pyaudio.PyAudio()
 AUDIO_STREAM = P_AUDIO.open(format=pyaudio.paFloat32,
                             channels=CHANNELS,
                             rate=RATE,
                             input=True,
                             output=True,
-                            frames_per_buffer=CHUNK_SIZE,
-                            stream_callback=callback)
-AUDIO_STREAM.start_stream()
+                            frames_per_buffer=CHUNK_SIZE)
 
 TG = m_tone_generator.ToneGenerator(RATE, 100)
 
@@ -48,8 +36,15 @@ def manipulate_stream(data_in):
 
     data_out = []
 
-    for _ in range(2048):
-        data_out.append(TG.output())
+    print(max(data_in), min(data_in))
+    print(len(data_in))
+
+    for _ in range(len(data_in)):
+        data_out.append(TG.output()*0.5)
+    # plt.plot(data_out)
+    # plt.show()
+    #print(max(data_out), min(data_out))
+    # data_out.append(0)
 
     # return the manipulated data
     return np.array(data_out)
@@ -61,11 +56,10 @@ print("running")
 try:
     while True:
         # get data, manipulate, then output
-        # DATA_IN = np.fromstring(
-        #    AUDIO_STREAM.read(CHUNK_SIZE), dtype=np.float32)
-        # DATA_OUT = manipulate_stream(DATA_IN)
-        # AUDIO_STREAM.write(DATA_OUT, CHUNK_SIZE)
-        pass
+        DATA_IN = np.fromstring(
+            AUDIO_STREAM.read(CHUNK_SIZE), dtype=np.float32)
+        DATA_OUT = manipulate_stream(DATA_IN)
+        AUDIO_STREAM.write(DATA_OUT, CHUNK_SIZE)
 except KeyboardInterrupt:
     print("keyboard interrupt")
 finally:
--- keller_dsp/m_tone_generator.py
+++ keller_dsp/m_tone_generator.py
@@ -20,13 +20,16 @@ class ToneGenerator:
         self.__fs = fs
         self.__f = f
         self.__phasak = 0
-        self.__maxint = 4294967295
+        self.__maxint = 44100
         self.__hz = self.__maxint / self.__fs
+        print(self.__hz)
 
     def output(self):
         """
         Takes one step and outputs new value.
         """
         self.__phasak = self.__phasak + self.__f * self.__hz
-        self.__phasak = self.__phasak % self.__maxint
-        return math.cos(2*math.pi*self.__phasak/self.__maxint)*100
+        #self.__phasak = self.__phasak % self.__maxint
+        if self.__phasak > self.__maxint:
+            self.__phasak = self.__phasak - self.__maxint
+        return math.cos(2*math.pi*self.__phasak/self.__maxint)
