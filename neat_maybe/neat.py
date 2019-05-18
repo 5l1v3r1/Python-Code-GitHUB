c53159275907e9697471e76aa5deb742391041b3
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-18 20:19:40 +0200

    added neat

---
+++ neat_maybe/neat.py
@@ -0,0 +1,40 @@
+"""
+Neat class.
+"""
+
+
+class Neat:
+    """
+    This will be a neat algorithm, I guess.
+    """
+
+    def __init__(self):
+        """
+        Parameters:
+        IN = Number of input nodes
+        OUT = Number of output nodes
+        HIDDEN = Number of hidden nodes
+        """
+
+    def advance(self):
+        """
+        Next step of network.
+        """
+        self.selection()
+        self.crossover()
+        self.mutation()
+
+    def selection(self):
+        """
+        Selects best.
+        """
+
+    def crossover(self):
+        """
+        Breeds new.
+        """
+
+    def mutation(self):
+        """
+        Slightly mutate childs.
+        """
